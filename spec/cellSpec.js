af61ee591740716528be5bfb7f71641e439720bc
pedro <pedro.tamone@nan-labs.com>
2017-10-26 11:13:50 -0300

    First test done

---
+++ spec/cellSpec.js
@@ -0,0 +1,13 @@
+describe("Cell", function() {
+  var cell;
+
+  beforeEach(function() {
+  	cell = new Cell();
+  })
+
+  it("should have a cell created", function() {
+
+    expect(cell).toBeDefined();
+  });
+  
+});
