071a74f0432a5cca53555301b388a0c0cea07dc6
fagarbal <fagarbal@gmail.com>
2017-10-26 16:09:36 +0200

    Cell

---
+++ spec/CellSpec.js
@@ -0,0 +1,22 @@
+describe("Cell", () => {
+	var cell;
+
+	beforeEach(() => {
+		cell = new Cell();
+	})
+
+	it("Should be an instance of Cell", () => {
+		expect(cell instanceof Cell).toEqual(true);
+	});
+
+	it("Should be alive", () => {
+		cell.setType(new Alive());
+		expect(cell.state instanceof Alive).toEqual(true);
+	});
+
+	it("Should be dead", () => {
+		cell.setType(new Dead());
+		expect(cell.state instanceof Dead).toEqual(true);
+	});
+
+})
